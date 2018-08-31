bda5698b852226ee8d4bc5ce822e4fb059ab93d8
Namrata <nam9503@gmail.com>
2018-08-31 17:55:25 +0530

    added methos getSpaceById

--- controllers/space.js
+++ controllers/space.js
@@ -1,29 +1,28 @@
 const Space = require('../models/Space')
 
 
-exports.getAllSpaces = (request,response) => {
+exports.getAllSpaces = (request, response) => {
 
-    Space.find({}, (error, spaces)=>{
+    Space.find({}, (error, spaces) => {
 
-        if(error)
-        {
+        if (error) {
             response.json({
-                message : "Server error, Please try after some time.",
-                status : 500
+                message: "Server error, Please try after some time.",
+                status: 500
             })
         }
-        if (spaces){
+        if (spaces) {
             response.json({
-                data : spaces,
-                message : "Space data fetched",
-                status : 200
+                data: spaces,
+                message: "Space data fetched",
+                status: 200
             })
         }
-        else{
+        else {
             response.json({
-              
-                message : "No data found",
-                status : 200
+
+                message: "No data found",
+                status: 200
             })
         }
 
@@ -32,17 +31,41 @@ exports.getAllSpaces = (request,response) => {
 
 }
 
-exports.getSpaceById = () =>{
+exports.getSpaceById = (request, response) => {
+
+    Space.findById( request.params.id , (error, spaces) => {
+
+        if(error){
+            response.json({
+            message : "Server error, Please try later.",
+            status : 404
+            })
+        }
+        if(spaces){
+            response.json({
+                data : spaces,
+                message : "Space id fetched",
+                status : 200
+            })
+        }
+        else{
+            response.json({
+                message : "Not found",
+                status : 200
+            })
+        }
+    })
+
 
 }
-exports.postCreateNewSpace = () =>{
+exports.postCreateNewSpace = () => {
 
 }
 
-exports.putUpdateSpace = () =>{
+exports.putUpdateSpace = () => {
 
 }
 
-exports.deleteSpace = () =>{
+exports.deleteSpace = () => {
 
 }
