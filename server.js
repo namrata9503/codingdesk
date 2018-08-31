ee31ab2aaffc93e7f930405ea124b16be2c6f17e
Namrata <nam9503@gmail.com>
2018-08-31 15:19:08 +0530

    made connection with mongoDB

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express')
-const app = express()
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost:27017/codingdesk')
+mongoose.connection.on('error', (error) => console.error(error))
+mongoose.connection.on('open',()=>console.log('successfully connected with mongodb..'))
+const app = express()
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
 
 app.get('/',(request,response)=>{
-    response.send("Hello World");
+    response.send("Hello World........");
 })
 
 
