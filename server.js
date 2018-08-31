2919f0183d84854535b80475469f1a0147153033
Namrata <nam9503@gmail.com>
2018-08-31 14:42:54 +0530

    added serevr

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+
+app.get('/',(request,response)=>{
+    response.json("Hello World");
+})
+
+
+app.listen(3000, () => console.log('Express server at 3000'))
