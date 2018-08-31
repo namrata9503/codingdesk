827ebc1e79cfd55be756b53c684209fb464a67ec
Namrata <nam9503@gmail.com>
2018-08-31 17:45:38 +0530

    created space controller with getAllSpaces function

--- controllers/space.js
+++ controllers/space.js
@@ -1,15 +1,45 @@
-exports.getSpaces = () => {
+const Space = require('../models/Space')
+
+
+exports.getAllSpaces = (request,response) => {
+
+    Space.find({}, (error, spaces)=>{
+
+        if(error)
+        {
+            response.json({
+                message : "Server error, Please try after some time.",
+                status : 500
+            })
+        }
+        if (spaces){
+            response.json({
+                data : spaces,
+                message : "Space data fetched",
+                status : 200
+            })
+        }
+        else{
+            response.json({
+              
+                message : "No data found",
+                status : 200
+            })
+        }
+
+    })
+
 
 }
 
 exports.getSpaceById = () =>{
 
 }
-exports.postNewSpace = () =>{
+exports.postCreateNewSpace = () =>{
 
 }
 
-exports.putSpace = () =>{
+exports.putUpdateSpace = () =>{
 
 }
 
--- server.js
+++ server.js
@@ -9,18 +9,23 @@ mongoose.connection.on('open', () => console.log('successfully connected with mo
 const app = express()
 
 const spaceController = require('./controllers/space')
-
+const userController = require()
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
 
 app.get('/', (request, response) => {
     response.send("Hello World........");
 })
-app.get('/api/v1/spaces', spaceController.getSpaces);
+
+app.get('/api/v1/spaces', spaceController.getAllSpaces);
 app.get('/api/v1/spaces/:id', spaceController.getSpaceById);
-app.post('/api/v1/spaces/new', spaceController.postNewSpace);
-app.put('/api/v1/spaces/:id',spaceController.putSpace);
+app.post('/api/v1/spaces/new', spaceController.postCreateNewSpace);
+app.put('/api/v1/spaces/:id',spaceController.putUpdateSpace);
 app.delete('/api/v1/spaces/:id', spaceController.deleteSpace)
+
+app.get('/api/v1/users, ')
 
 
 app.listen(3000, () => console.log('Express server at 3000'))
+
+
